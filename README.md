5ed2bc26eed537c04da1b56de062a48fd82ae91a
pardocch <my900002@gmail.com>
2020-05-05 22:41:12 +1000

    Dcard-bnner-removal

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dcard-banner-removal
+
+** This extension will remove the pop-up banner on the Dcard while it is not logged in. **
---
+++ dcard-banner-removal.js
@@ -0,0 +1,7 @@
+let banner = ""
+setInterval(() => {
+    banner =  (document.querySelector(".fMNFMJ")) ? document.querySelector(".fMNFMJ") : ""
+    if (banner) {
+        banner.style.display = "none";
+    }
+}, 3000)
